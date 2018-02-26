a26135701d9176350eb40652ac2059a2c5426067
Yuhan <yuhan.lee@bitesite.ca>
2018-02-25 22:37:19 -0500

    fixed firebase authentication, signed-in

--- app/src/main/java/plscuddleme/yuhanlee/cremebrulee/DashBoardActivity.java
+++ app/src/main/java/plscuddleme/yuhanlee/cremebrulee/DashBoardActivity.java
@@ -1,12 +1,18 @@
 package plscuddleme.yuhanlee.cremebrulee;
 
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 /**
  * Created by yuhanlee on 2018-02-24.
  */
 
-public class DashBoardActivity {
+
+public class DashBoardActivity extends AppCompatActivity implements View.OnClickListener{
 
 
+    @Override
+    public void onClick(View v) {
 
-
+    }
 }
